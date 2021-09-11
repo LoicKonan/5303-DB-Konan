7d21ca138b0c550dd09ef5b58fb0f6bae7ca4787
Loic <konanloic10@gmail.com>
2021-09-10 21:35:22 -0500

    README.md

--- Assignments/A04/SELECT_basics.sql
+++ Assignments/A04/SELECT_basics.sql
@@ -1,5 +1,8 @@
-SELECT population FROM world WHERE name = 'Germany';
+SELECT population FROM world
+  WHERE name = 'Germany'
 
-SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway','Denmark');
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
-SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000 
