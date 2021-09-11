da4a937e21195b9f58148ba97d765b0268a07408
Loic <konanloic10@gmail.com>
2021-09-10 22:02:34 -0500

    README.md

--- Assignments/A04/SELECT_from_WORLD.sql
+++ Assignments/A04/SELECT_from_WORLD.sql
@@ -2,7 +2,7 @@ SELECT name, continent, population FROM world;
 
 SELECT name FROM world WHERE population >= 200000000;
 
-SELECT name,gdp FROM world WHERE population >= 200000000;
+SELECT name, gdp / population FROM world WHERE population >= 200000000;
 
 SELECT name, population/1000000 FROM world WHERE continent = 'South America';
 
