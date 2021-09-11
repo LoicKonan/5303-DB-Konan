ed00d757a5a57a36672943aa376699d6b02badac
Loic <konanloic10@gmail.com>
2021-09-11 00:15:05 -0500

    README.md

--- Assignments/A04/SELECT_within_SELECT.sql
+++ Assignments/A04/SELECT_within_SELECT.sql
@@ -9,15 +9,15 @@ SELECT name FROM world WHERE population > (SELECT population FROM world WHERE na
 
 SELECT name FROM world WHERE gdp/population > (SELECT gdp/population FROM world WHERE name = 'United Kingdom') AND continent = 'Europe';
 
-SELECT name,continent FROM world WHERE continent = (SELECT continent FROM world WHERE name = 'Argentina ') OR continent = (SELECT continent FROM world WHERE name = 'Australia') ORDER BY name;
+SELECT name, continent FROM world WHERE continent = (SELECT continent FROM world WHERE name = 'Argentina ') OR continent = (SELECT continent FROM world WHERE name = 'Australia') ORDER BY name;
 
-SELECT name,population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland');
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland');
 
 SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany')* 100),'%')  AS percentage FROM world WHERE continent = 'Europe';
 
 SELECT name FROM world WHERE gdp > ALL(SELECT gdp FROM world WHERE continent = 'Europe' AND gdp IS NOT null);
 
-SELECT continent, name, area FROM world x WHERE area >= ALL (SELECT area FROM world y WHERE y.continent=x.continent AND area>0);
+SELECT continent, name, area FROM world x WHERE area >= ALL (SELECT area FROM world y WHERE y.continent = x.continent AND area > 0);
 
 SELECT continent, name FROM world x WHERE name <= ALL(SELECT name FROM world y WHERE x.continent = y.continent);
 
