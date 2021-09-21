35dd10247c85920c963d51b49bfad9a5bfa5ae3e
Loic <konanloic10@gmail.com>
2021-09-20 23:27:25 -0500

    README.md

--- Assignments/A04/Sql_Zoo_Answers/SELECT_from_Nobel.sql
+++ Assignments/A04/Sql_Zoo_Answers/SELECT_from_Nobel.sql
@@ -9,7 +9,7 @@ SELECT yr, subject, winner FROM nobel WHERE yr = 1950;
 
 SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'Literature';
 
-SELECT yr,subject FROM nobel WHERE winner = 'Albert Einstein';
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
 
 SELECT winner FROM nobel WHERE subject = 'Peace' AND yr >= 2000;
 
