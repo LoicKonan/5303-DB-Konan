fc54373da554e045bf00b315c5624c019e01c744
Loic <konanloic10@gmail.com>
2021-12-01 20:44:01 -0600

    README.md

--- Assignments/A08/get_data.php
+++ Assignments/A08/get_data.php
@@ -1,6 +1,7 @@
 <?php
 
-for($i = 0;$i<100;$i++){
+for($i = 0;$i<100;$i++)
+{
     $data = file_get_contents("https://api.mockaroo.com/api/f52ae350?count=1000&key=ca0d25f0");
     if($i<10){
         $name = "000{$i}";
